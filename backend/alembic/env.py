16f3403d3d20fc243beefc1f5dee14bb1e0c38ac
Yasara Samaraweera <yasarasamaraweera@gmail.com>
2026-02-06 02:24:55 +0530

    Update authentication routes and service and minor related adjustmens

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,9 +1,20 @@
 from logging.config import fileConfig
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src")))
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+from app.database import Base
+from app.models.user import User
+from app.models.worker import Worker, WorkerDocument
+from app.models.employer import Employer
+from app.models.job import Job
+from app.models.application import Application
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +29,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- backend/src/app/routes/auth.py
+++ backend/src/app/routes/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 import logging
 
