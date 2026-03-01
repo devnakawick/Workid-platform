a0fd033183e361bd7e9ffe3bb4a808f6748df11f
rashmi-perera <rashmi.20241540@iit.ac.lk>
2026-03-01 18:40:32 +0530

    init applicants list

---
+++ frontend/src/components/employer/ApplicantsList.jsx
@@ -0,0 +1,11 @@
+import { FileX, CircleCheck } from "lucide-react";
+
+const ApplicantsList = ({ applications, selected, stats, onSelect }) => {
+  return (
+    <div className="w-72 bg-white border-r border-gray-200 flex flex-col flex-shrink-0 h-full">
+      <p>Applicants List</p>
+    </div>
+  );
+};
+
+export default ApplicantsList;
