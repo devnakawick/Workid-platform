b083e6a99566be992f1cb0a2c9247df4c5d85a7f
Yasara Samaraweera <yasarasamaraweera@gmail.com>
2026-03-07 01:05:54 +0530

    Export EmployerService

--- backend/src/app/services/__init__.py
+++ backend/src/app/services/__init__.py
@@ -1,11 +1,13 @@
 from app.services.file_service import FileService
 from app.services.worker_service import WorkerService
+from app.services.employer_service import EmployerService
 from app.services.job_service import JobService
 from app.services.auth_service import AuthService
 
 __all__ = [
     "FileService",
     "WorkerService",
+    "EmployerService",
     "JobService",
     "AuthService"
 ]
